dff7ca3d7d391ed2ba69a821e53eee63c5090e3a
aryanarora07 <aroraaryan824@gmail.com>
2024-07-09 04:24:30 -0700

    ll

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const express = require('express');
 const app  = express();
 const http = require("http");
 
+const path = require('path');
+
 const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirnamedirname, "public")));
+app.set(express.static(path.join(__dirname, "public")));
 
-
-app.get("/", (req, res) =>{
-    res.send("hey");
+io.on("connection", function(socket){
+    console.log("connected");
 });
 
-server.listen(3000);
+app.get("/", (req, res) =>{
+    res.render("index");
+});
+
+server.listen(3000); 
