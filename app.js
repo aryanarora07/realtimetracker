abe9f3d79cd19feb01cd640bd8c9dcfed28f17a7
aryanarora07 <aroraaryan824@gmail.com>
2024-07-09 03:58:05 -0700

    express blrplt

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app  = express();
+const http = require("http");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirnamedirname, "public")));
+
+
+app.get("/", (req, res) =>{
+    res.send("hey");
+});
+
+server.listen(3000);
