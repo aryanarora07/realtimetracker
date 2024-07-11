0e5fc5708bdfb3f6d94fd65ff5bc9c5761a3116f
aryanarora07 <aroraaryan824@gmail.com>
2024-07-10 21:08:53 -0700

    working now

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const express = require('express');
-
-const app  = express();
-const http = require("http");
-
+const express = require("express");
+const app = express();
 const path = require('path');
-
+const http = require("http");
 const socketio = require("socket.io");
+
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.join(__dirname, "public")));  // Fixed this line
 
 io.on("connection", function(socket){
     console.log("connected");
 });
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.render("index");
 });
 
-server.listen(3000); 
+server.listen(3000, () => {  // Added a callback to confirm the server is running
+    console.log("Server running on port 3000");
+});
